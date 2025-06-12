02d79f79e15e2d5a76610f5b9e9110099359ed91
ben <benjamin.zelinsky@gmail.com>
2025-06-12 15:08:56 +0200

    Rename variables in Pokemon to match better.

--- src/pokemonTypes/Pokemon.java
+++ src/pokemonTypes/Pokemon.java
@@ -4,25 +4,40 @@ public class Pokemon {
 
     // Properties
     private String species;
-    private int weight;
-    private int height;
+    private int level;
+    private int hp;
     private String name;
-    private String cry;
-
+    private String sound;
 
     // Constructor
 
 
-    public Pokemon(String species, int weight, int height, String name, String cry) {
+    public Pokemon(String species, int level, int hp, String name, String sound) {
         this.species = species;
-        this.weight = weight;
-        this.height = height;
+        this.level = level;
+        this.hp = hp;
         this.name = name;
-        this.cry = cry;
+        this.sound = sound;
     }
 
-    // Getters & Setters
+    // Getters
     public String getName() {
         return this.name;
+    }
+
+    public String getSpecies() {
+        return this.species;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public String getSound() {
+        return this.sound;
     }
 }
