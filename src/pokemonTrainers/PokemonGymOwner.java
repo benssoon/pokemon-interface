6c0e9d4a6ab5a5307fa125eeaa22891ebe50c67a
ben <benjamin.zelinsky@gmail.com>
2025-06-12 15:15:16 +0200

    Fix order in constructor. Add getTown.

--- src/pokemonTrainers/PokemonGymOwner.java
+++ src/pokemonTrainers/PokemonGymOwner.java
@@ -1,14 +1,18 @@
 package pokemonTrainers;
 
-import pokemonTypes.Pokemon;
+import pokemonTypes.*;
 
 import java.util.List;
 
 public class PokemonGymOwner extends PokemonTrainer {
     private String town;
 
-    public PokemonGymOwner(String name, List<Pokemon> pokemons, String town) {
+    public PokemonGymOwner(String name, String town, List<Pokemon> pokemons) {
         super(name, pokemons);
         this.town = town;
+    }
+
+    public String getTown() {
+        return this.town;
     }
 }
