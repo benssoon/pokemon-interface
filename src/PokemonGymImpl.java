058e8cdae8e1c1a779b8408f796d611d01137aed
ben <benjamin.zelinsky@gmail.com>
2025-06-12 22:19:03 +0200

    Import java.util.*.

--- src/PokemonGymImpl.java
+++ src/PokemonGymImpl.java
@@ -1,10 +1,8 @@
 import pokemonTrainers.PokemonGymOwner;
 import pokemonTrainers.PokemonTrainer;
 import pokemonTypes.*;
-import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Random;
+
+import java.util.*;
 
 // Los in deze klasse alle foutmeldingen op door (abstracte) klassen met variabelen en methodes te maken en een interface met methodes (en soms een import).
 public class PokemonGymImpl implements PokemonGym {
